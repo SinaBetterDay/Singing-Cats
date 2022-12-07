cb079307bce69396d03d5150337b8e5e88da4f87
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2022-12-07 02:26:08 -0800

    Update Engine.h

--- code/Engine.h
+++ code/Engine.h
@@ -1,22 +1,46 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 using namespace sf;
 
 class Engine
 {
 private:
-
     Sprite backgroundSprite;
     Texture backgroundTexture;
 	
 public:
-	// The Engine constructor
-	Engine();
-    RenderWindow window;
-	void guitarCat();
+	RenderWindow window;
 	void animationDelay();
+
+    int flicker1, flicker2, flicker3, flicker4, flicker5, flicker6 = 0;
+    Clock clock;
 	// Run will call all the private functions
 	void run();
 
+    void pianoCat();
+	void guitarCat();
+	void drumCat();
+	void bongoCat();
+	void fluteCat();
+	void popCat();
+
+    bool foo1, foo2, foo3, foo4, foo5, foo6 = false;
+	bool isplay1, isplay2, isplay3, isplay4, isplay5, isplay6 = false;
+    SoundBuffer guitarBuffer, pianoBuffer, drumBuffer, bongoBuffer, popBuffer, rickrollBuffer;
+    Sound guitarsound, pianosound, drumsound, bongosound, popsound, rickrollsound;
+
 };
+
+
+/*
+class GuitarCat : public Engine
+{
+public:
+	void guitarCat();
+	Texture texture;
+	Sprite sprite;
+
+};
+*/
